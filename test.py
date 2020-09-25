4766fe0c7e3c4e143ff01646bb70219f05e24e5d
Ege Ata Türküler <ege.turkuler@ozu.edu.tr>
2020-09-25 17:17:17 +0300

    hmm

---
+++ test.py
@@ -0,0 +1,14 @@
+import time
+
+msg = "Guess who's back ?"
+msg2 = "Back again.."
+msg3 = "Ege is back"
+msg4 = "Tell a friend"
+print(msg)
+time.sleep(2)
+print(msg2)
+time.sleep(2)
+print(msg3)
+time.sleep(2)
+print(msg4)
+
